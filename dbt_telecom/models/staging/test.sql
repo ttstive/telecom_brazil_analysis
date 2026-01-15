221729c697d3ffbea7bb22ee885dbed663c4d7d7
Estevão Maia <estevao.maia@a.ucb.br>
2026-01-14 21:45:15 -0300

    finalizando e adicionando test do dbt, e gerando documentacao com serve na port 9001

--- dbt_telecom/models/staging/test.sql
+++ dbt_telecom/models/staging/test.sql
@@ -1,1 +1,19 @@
-SELECT * FROM raw_ibc_uf_indicadores_originais LIMIT 100
+WITH source AS (
+    SELECT *
+    FROM raw_ibc_uf_indicadores_originais
+),
+
+renamed AS (
+    SELECT
+        CAST("ï»¿Ano" AS INT)                    AS ano,
+        UF,
+        INDICADOR,
+        CAST("Cobertura Pop. 4G5G" AS DOUBLE)   AS cobertura_pop_4g5g,
+        CAST("Adensamento EstaÃ§Ãµes" AS DOUBLE) AS adensamento_estacoes,
+        CAST("Cobertura Ã¡rea agricultÃ¡vel" AS DOUBLE) AS cobertura_area_agricutavel
+    FROM source
+)
+
+SELECT *
+FROM renamed
+
